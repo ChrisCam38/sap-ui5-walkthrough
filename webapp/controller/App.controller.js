801c436a0758de9471767b781fb3cfae553bbc6d
ChrisCam38 <kristiantilano@gmail.com>
2025-07-22 21:27:20 -0500

    Step 8 - Traslatable Text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onInit: function () {
@@ -20,13 +21,25 @@ sap.ui.define(
 
         //3. Asignamos el modelo a la vista
         this.getView().setModel(oModel);
+
+        //Set i18n model on view
+        var i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          falbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
 
       onShowHello: function () {
-        MessageToast.show(
-          //Show a messagetoast
-          "Hola, vamos progresando"
-        );
+        //Read message from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        //Show message
+        MessageToast.show(sMsg);
       },
     });
   }
